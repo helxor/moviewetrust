1bec4c168144e534339b9c12948a6a33b0037c98
vndrewboss <33851645+vndrewboss@users.noreply.github.com>
2019-09-05 00:57:15 +0800

    Update Movie.js

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -393,11 +393,9 @@ const Movie = ({
         </MovieWrapper>
        </LazyLoad>
       <MovieContainer>
-{renderMovie1(movie.imdb_id)}
-      </MovieContainer>
-      <MovieContainer>
       {renderMovie(movie.imdb_id)}
       </MovieContainer>
+                            {renderMovie1(movie.imdb_id)}
       <Header title="Recommended" subtitle="movies" />
       {renderRecommended(recommended, secure_base_url)}
     </Wrapper>
@@ -444,9 +442,9 @@ function renderMovie(id) {
     return null;
   }
   return (
-
  <MovieA>
     <center>
+                                    <Text>Reminder: Make sure that you install the recommended extensions to stream and download movies here! <br /> HOW TO? Restart the url, Read the pop-up instructions! Thank you.</Text>
      <iframe src={`https://videospider.in/getvideo?key=JzAFyoTAtzAwVCXW&video_id=${id}`} width="800" height="400" frameborder="0" allowfullscreen="true"></iframe>
 </center>
 </MovieA>
@@ -459,6 +457,7 @@ function renderMovie1(id) {
   }
   return (
     <center>
+    <Text>SPONSORED ADVERTISEMENT</Text>
 <iframe src={`https://d22sfab2t5o9bq.cloudfront.net/br?bafsd=805970`} width="800" height="150" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-top-navigation"></iframe>
 </center>
   );
